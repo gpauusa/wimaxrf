cb931c7c946b953cc6d79a3f7880e1285d529177
Davide Pesavento <davidepesa@gmail.com>
2013-06-12 20:05:00 -0700

    dpClick1: fix super invocation.

--- dpClick1.rb
+++ dpClick1.rb
@@ -17,11 +17,11 @@ class Click1Datapath < DataPath
   end
 
   def initialize(config = {})
-   super
+   super()
    @vlan = config['vlan'] || 0
    @port = config['eth_port'] || "eth0"
    @def_gw = config['def_gw'] || "10.41.0.1"
-   @net_mask = config['net_mask']  || "255.255.0.0"
+   @net_mask = config['net_mask'] || "255.255.0.0"
    @def_ip = config['def_ip'] || "10.41.0.254"
    @click_command = config['click_command'] || '/usr/local/bin/click'
    @click_conf = config['click_conf'] || '/tmp/Wimax.click'
