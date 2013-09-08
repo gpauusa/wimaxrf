2c903934227ea8863ad62761cc12c12419bd207b
Fraida Fund <ffund@nyu.edu>
2013-09-08 16:25:52 -0400

    Fix in modify client (macaddr to client.macaddr)

--- wimaxrf.rb
+++ wimaxrf.rb
@@ -929,7 +929,7 @@ class WimaxrfService < LegacyGridService
       if client.vlan != vlan || client.interface != interface
         updates[:vlan] = vlan
         updates[:interface] = interface
-        message << "Datapath for #{macaddr} updated"
+        message << "Datapath for #{client.macaddr} updated"
       end
     else
       message << "Cannot modify vlan/interface, datapath #{interface}-#{vlan} does not exist"
@@ -938,12 +938,12 @@ class WimaxrfService < LegacyGridService
     # prepare ip address change
     if ipaddress != nil && ipaddress != client.ipaddress
       updates[:ipaddress] = ipaddress
-      message << "\nIP address for #{macaddr} updated"
+      message << "\nIP address for #{client.macaddr} updated"
     end
 
     if !updates.empty?
       # apply changes
-      @auth.update_client(macaddr, updates)
+      @auth.update_client(client.macaddr, updates)
     end
     message
   end
